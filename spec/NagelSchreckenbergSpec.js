1798054f24383a25707a9659a00dc29586cdc2d2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-05-29 15:45:53 +0200

    Car Specification
    The Car specification is fleshed out.

--- spec/NagelSchreckenbergSpec.js
+++ spec/NagelSchreckenbergSpec.js
@@ -19,4 +19,32 @@ describe("Car Model", function(){
     it("should have a default maximum speed of 5", function(){
 	expect(car.get("maximum_speed")).toBe(5);
     });
+
+    it("should have a default current speed of 0", function(){
+	expect(car.get("current_speed")).toBe(0);
+    });
+    
+    describe("speed increase", function(){
+    	it("should go from 0 to 1", function(){
+	    car.increaseSpeed();
+	    
+	    expect(car.get("current_speed")).toBe(1);
+	});
+    
+    	it("should go from 4 to 5", function(){
+	    car.set({ "current_speed" : 4 });
+
+	    car.increaseSpeed();
+	    
+	    expect(car.get("current_speed")).toBe(5);
+	});
+    	
+	it("should not go beyond maximum speed", function(){
+	    car.set({ "current_speed" : car.get("maximum_speed") });
+
+	    car.increaseSpeed();
+	    
+	    expect(car.get("current_speed")).toBe(car.get("maximum_speed"));
+	});    
+    });
 });
--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,14 @@
 (function(_, Backbone, undefined){
     var CarModel = Backbone.Model.extend({
 	defaults : {
-	    "maximum_speed" : 5
+	    "maximum_speed" : 5,
+	    "current_speed" : 0,
+	},
+
+	increaseSpeed : function(){
+	    var last_speed = this.get("current_speed");
+	    var maximum_speed = this.get("maximum_speed");
+	    this.set({ "current_speed" : Math.min(last_speed + 1, maximum_speed) });
 	}
     });
 
